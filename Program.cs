bd6b6dd1aec4d1fdd90ad6dd15d5f7ffc286b53a
cybersi-io <66743889+cybersi-io@users.noreply.github.com>
2021-02-27 17:09:46 +0000

    Initial commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace Palindrome_Checker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a word...");
+            string word = Console.ReadLine();
+            char[] wordArray = word.ToCharArray();
+            Array.Reverse(wordArray);
+            string reverseWord = String.Concat(wordArray);
+
+            if (word == reverseWord)
+            {
+                Console.WriteLine("This is a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("This is NOT a plaindrome");
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
